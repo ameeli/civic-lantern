80fbd8f22e308242e773a255a30eefdc5395a0ee
Amee Li <ameejli@gmail.com>
2026-03-27 16:43:24 -0700

    style(frontend): add sky sun background

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,8 @@ import PaperBorder from "@/components/PaperBorder";
 
 export default function Home() {
   return (
-    <div className="relative flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="relative flex flex-1 w-full max-w-6xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+    <div className="relative flex flex-col flex-1 items-center justify-center bg-sky-sun font-sans dark:bg-black">
+      <main className="relative flex flex-1 w-full max-w-5xl flex-col items-center justify-between py-32 px-16 sm:items-start drop-shadow-xl">
         <PaperBorder />
         <div className="relative z-10 flex"></div>
       </main>
