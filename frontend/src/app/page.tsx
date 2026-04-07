2595ba79b24a950f2744c335c3f37a6cca71fdfc
Amee Li <ameejli@gmail.com>
2026-04-07 11:08:58 -0700

    style(frontend): use grid for layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,13 +3,13 @@ import PaperBorder from "@/components/PaperBorder";
 
 export default function Home() {
   return (
-    <div className="relative flex flex-col flex-1 items-center justify-center py-5 bg-dark-wood font-sans dark:bg-black">
-      <main className="relative flex flex-1 w-full max-w-4xl flex-col items-center justify-between py-16 px-12 sm:items-start">
+    <div className="flex flex-col flex-1 items-center py-5 bg-dark-wood font-sans dark:bg-black">
+      <main className="relative grid grid-cols-6 gap-1 flex-1 w-full max-w-4xl py-16 px-12">
         <PaperBorder />
-        <div className="relative flex w-full justify-center">
+        <div className="col-span-6 flex flex-col items-center gap-4">
           <h1 className="text-masthead text-3xl">The Civic Lantern</h1>
+          <Gavel width={40} height={40} />
         </div>
-        <Gavel width={200} height={200} className=" relative" />
       </main>
     </div>
   );
