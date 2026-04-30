61f82a7c228f8bd5eefc64607afebae90b463d37
Amee Li <ameejli@gmail.com>
2026-04-30 14:30:07 -0700

    refactor(backend): remove unused models

--- backend/civic_lantern/db/models/__init__.py
+++ backend/civic_lantern/db/models/__init__.py
@@ -1,24 +1,12 @@
 from .base import Base, ViewBase
 from .candidate import Candidate
 from .candidate_spending import CandidateSpendingTotals
-from .committee import Committee
-from .election import Election
-from .election_candidate import ElectionCandidate
 from .election_spending import MvElectionSpendingSummary
-from .schedule_e import ScheduleE
-from .totals_by_candidate import TotalsByCandidate
-from .totals_by_election import TotalsByElection
 
 __all__ = [
     "Base",
     "ViewBase",
     "Candidate",
     "CandidateSpendingTotals",
-    "Committee",
-    "Election",
-    "ElectionCandidate",
     "MvElectionSpendingSummary",
-    "ScheduleE",
-    "TotalsByCandidate",
-    "TotalsByElection",
 ]
