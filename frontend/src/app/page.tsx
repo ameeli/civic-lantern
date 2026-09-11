e252a0f602ab5c05744befd8023639dbc74f1a06
Amee Li <ameejli@gmail.com>
2026-09-11 11:09:41 -0700

    style(frontend): padding without paper border

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -87,12 +87,12 @@ export default async function Home({ searchParams }: PageProps<"/">) {
   return (
     <div className="relative isolate flex flex-col flex-1 items-center bg-dark-wood font-sans">
       <PaperBorder />
-      <main className="grid grid-cols-12 content-start gap-x-4 gap-y-1 w-full max-w-7xl p-12 lg:p-15 ">
+      <main className="grid grid-cols-12 content-start gap-x-4 gap-y-1 w-full max-w-6xl px-12 py-7 lg:px-15 lg:py-9">
         <Masthead cycles={readyCycles} selectedCycle={cycle} />
         <div className="col-span-12 mb-3 flex flex-col items-center gap-2">
           <h1 className="font-headline font-semibold text-3xl text-center">
-            Candidate Cash vs. Super PAC Millions: Who Truly Controls the
-            Election Narrative?
+            Candidate vs. Super PAC Cash: Who Truly Controls the Election
+            Narrative?
           </h1>
         </div>
         <ElectionSpendingSection cycle={cycle} />
